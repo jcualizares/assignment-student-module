203252a4ecd2b3b8b000db1ca76f98cb519b744c
Paner <168882211+jcualizares@users.noreply.github.com>
2025-01-11 22:56:12 +0800

    main student

---
+++ src/student/student.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { studentService } from "./student.service";
+
+//controller
+//dito kinocontrol yon mga ginagawa sa create-to.dto.ts
+
+@Controller('student')
+export class StudentController{
+    constructor(private readonly studentService: studentService){}
+  
+  
+
+ 
+
+    
+   
+   
+
+
+
+
+
+}
